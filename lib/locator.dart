88106e58abfd5af435f91ff53d6307558aacb9c3
John Russel Garcia <30710854+johnrusselmoleragarcia@users.noreply.github.com>
2020-02-08 17:01:07 +0800

    Added AuthServ locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,6 @@
 import 'package:pasabay_app/services/authentication_service.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pasabay_app/services/firestore_service.dart';
 import 'package:pasabay_app/services/navigation_service.dart';
 import 'package:pasabay_app/services/dialog_service.dart';
 
@@ -9,4 +10,5 @@ void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => DialogService());
   locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => FirestoreService());
 }
