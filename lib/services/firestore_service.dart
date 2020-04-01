c07e7d88124651edc341934c265b1e4643a3acad
John Russel Garcia <30710854+johnrusselmoleragarcia@users.noreply.github.com>
2020-04-01 19:33:00 +0800

    Remove unused functions in firestore_service

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -10,9 +10,6 @@ class FirestoreService {
   final CollectionReference _postsCollectionReference =
       Firestore.instance.collection('posts');
 
-  final StreamController<List<Post>> _postsController = 
-      StreamController<List<Post>>.broadcast();
-
   Future createUser(User user) async {
     try {
       await _usersCollectionReference.document(user.uid).setData(user.toJson());
@@ -30,35 +27,6 @@ class FirestoreService {
     try {
       await _postsCollectionReference.add(post.toMap());
     } catch (e) {}
-  }
-
-  Future getPostsOnceOff() async {
-    try {
-      var postDocumentSnapshot = await _postsCollectionReference.getDocuments();
-      if (postDocumentSnapshot.documents.isNotEmpty) {
-        return postDocumentSnapshot.documents
-            .map((snapshot) => Post.fromMap(snapshot.data, snapshot.documentID))
-            .where((mappedItem) => mappedItem.title != null)
-            .toList();
-      }
-    } catch (e) {}
-  }
-
-  Stream listenToPostsRealTime() {
-    // Register the handler for when the posts data changes
-    _postsCollectionReference.snapshots().listen((postsSnapshot) {
-      if (postsSnapshot.documents.isNotEmpty) {
-        var posts =  postsSnapshot.documents
-            .map((snapshot) => Post.fromMap(snapshot.data, snapshot.documentID))
-            .where((mappedItem) => mappedItem.title != null)
-            .toList();
-
-        // Add the posts onto the controller
-        _postsController.add(posts);
-      }
-    });
-
-    return _postsController.stream;
   }
 
   Future updatePost(Post post) async {
