0244ced064a29eceab4cd2bcd6bb48955340fa23
John Russel Garcia <jmgarcia9@up.edu.ph>
2020-05-16 08:14:15 +0800

    Added limit post dialog on Dashboard

--- lib/viewmodels/dashboard_view_model.dart
+++ lib/viewmodels/dashboard_view_model.dart
@@ -1,14 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pasabay_app/constants/category_names.dart';
 import 'package:pasabay_app/constants/route_names.dart';
 import 'package:pasabay_app/locator.dart';
+import 'package:pasabay_app/services/authentication_service.dart';
+import 'package:pasabay_app/services/dialog_service.dart';
 import 'package:pasabay_app/services/navigation_service.dart';
 import 'package:pasabay_app/viewmodels/base_model.dart';
 
 class DashboardViewModel extends BaseModel {
+  final DialogService _dialogService = locator<DialogService>();
   final NavigationService _navigationService = locator<NavigationService>();
+  final AuthenticationService _authenticationService = locator<AuthenticationService>();
 
   Future navigateToCreateView() async {
-    await _navigationService.navigateTo(CreatePostViewRoute);
+    QuerySnapshot postCount = await Firestore.instance.collection('posts')
+      .where('userId', isEqualTo: _authenticationService.currentUser.uid)
+      .where('fulfilledBy', isNull: true)
+      .getDocuments();
+
+    if (postCount.documents.length < 5) {
+      await _navigationService.navigateTo(CreatePostViewRoute);
+    } else {
+      await _dialogService.showDialog(
+        title: "Limit Post",
+        description: "You are only allowed to create up to five posts."
+      );
+    }
   }
 
   Future browseCleaning() async {
